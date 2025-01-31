92faf6df50b60792c7e01ae8a6a22c262b8eaffb
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2025-01-31 09:35:52 +0100

    djkd

---
+++ day 1.py
@@ -0,0 +1,12 @@
+#AdventOfCode-Challenge day 1 part 1
+#For reading the lines each side and store them in seperate lines
+
+left =[]
+right = []
+
+with open("my_data.txt", encoding='utf-8') as file:
+    for line in file:
+        nums = line.strip().split()
+        left.append(int(nums[0]))
+        right.append(int(nums[1]))
+
